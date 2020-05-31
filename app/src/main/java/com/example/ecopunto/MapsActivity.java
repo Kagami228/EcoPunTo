3d6077165a4a0cc9d7f7daa43fb0d3f6ce31565b
Kagami228 <sasha.dovgan@gmail.com>
2020-05-31 15:07:34 +0300

    initial commit

---
+++ app/src/main/java/com/example/ecopunto/MapsActivity.java
@@ -0,0 +1,241 @@
+package com.example.ecopunto;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Objects;
+import java.util.Vector;
+
+import javax.annotation.Nullable;
+import io.opencensus.common.Function;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private static final String TAG = "MapActivity";
+
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    private static final float DEFAULT_ZOOM = 15f;
+
+    //vars
+    private Boolean mLocationPermissionsGranted = false;
+    private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private FirebaseFirestore mFirestore;
+    public Vector<LatLng> trashVectorLng;
+    private Data docSnippets;
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Toast.makeText(this, "Map is Ready", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onMapReady: map is ready");
+        mMap = googleMap;
+
+        if (mLocationPermissionsGranted) {
+            getDeviceLocation();
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            mMap.setMyLocationEnabled(true);
+            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+            initMapData();
+        }
+    }
+
+
+    public BitmapDescriptor getMarkerIcon(String color) {
+        float[] hsv = new float[3];
+        Color.colorToHSV(Color.parseColor(color), hsv);
+        return BitmapDescriptorFactory.defaultMarker(hsv[0]);
+    }
+
+    //
+    private void initMapData(){
+
+        try {
+            docSnippets.getAllTrash( new Function<Data.Trash,Boolean>(){
+                @Override
+                public Boolean apply(Data.Trash trash) {
+                    {
+                        trashVectorLng= new Vector<>();
+                      //  trashVector.add(trash);
+                        double lng=trash.lat;
+                        double longs =trash.Long;
+                        LatLng TrashLatLng = new LatLng(lng,longs);
+                        trashVectorLng.add(TrashLatLng);
+                        if(trash.event_type.equals("Empty")){
+
+                            if(trash.box_type.equals("Paper")){
+                                mMap.addMarker(new MarkerOptions().position(TrashLatLng).icon(BitmapDescriptorFactory
+                                        .defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)).title(trash.box_type+" Empty"));
+                            }
+                            if(trash.box_type.equals("Metal")){
+                                mMap.addMarker(new MarkerOptions().position(TrashLatLng).icon(BitmapDescriptorFactory.defaultMarker(306 )).title(trash.box_type+" Empty"));
+                            }
+                            if(trash.box_type.equals("Bio")){
+                                mMap.addMarker(new MarkerOptions().position(TrashLatLng).icon(BitmapDescriptorFactory
+                                        .defaultMarker(BitmapDescriptorFactory.HUE_GREEN)).title(trash.box_type+" Empty"));
+                            }
+                            if(trash.box_type.equals("Plastik")){
+                                mMap.addMarker(new MarkerOptions().position(TrashLatLng).icon(BitmapDescriptorFactory.defaultMarker(35 )).title(trash.box_type+" Empty"));
+                            }
+                            if(trash.box_type.equals("Glass")){
+                                mMap.addMarker(new MarkerOptions().position(TrashLatLng).icon(BitmapDescriptorFactory.defaultMarker(197 )).title(trash.box_type+" Empty"));
+                            }
+
+
+                        }else {
+                            mMap.addMarker(new MarkerOptions().position(TrashLatLng).icon(BitmapDescriptorFactory
+                                    .defaultMarker(BitmapDescriptorFactory.HUE_RED)).title(trash.box_type + " Full"));
+                        }
+                        return true;
+
+                    }
+                }
+            });
+
+        }catch (Exception e){
+            Log.d(TAG, Objects.requireNonNull(e.getMessage()));
+
+        }
+    }
+
+    //
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        ///////
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        mFirestore = FirebaseFirestore.getInstance();
+        docSnippets = new Data(mFirestore);
+        getLocationPermission();
+    }
+
+    //
+
+    private void getDeviceLocation(){
+        Log.d(TAG, "getDeviceLocation: getting the devices current location");
+
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        try{
+            if(mLocationPermissionsGranted){
+
+                final Task location = mFusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if(task.isSuccessful()){
+                            Log.d(TAG, "onComplete: found location!");
+                            Location currentLocation = (Location) task.getResult();
+
+                            assert currentLocation != null;
+                            moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()),
+                                    DEFAULT_ZOOM);
+
+                        }else{
+                            Log.d(TAG, "onComplete: current location is null");
+                            Toast.makeText(MapsActivity.this, "unable to get current location", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        }catch (SecurityException e){
+            Log.e(TAG, "getDeviceLocation: SecurityException: " + e.getMessage() );
+        }
+    }
+
+    //
+
+    private void moveCamera(LatLng latLng, float zoom){
+        Log.d(TAG, "moveCamera: moving the camera to: lat: " + latLng.latitude + ", lng: " + latLng.longitude );
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+    }
+
+    //
+
+    private void initMap(){
+        Log.d(TAG, "initMap: initializing map");
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+        assert mapFragment != null;
+        mapFragment.getMapAsync(MapsActivity.this);
+    }
+
+    //
+
+    private void getLocationPermission(){
+        Log.d(TAG, "getLocationPermission: getting location permissions");
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION};
+
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                    COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                mLocationPermissionsGranted = true;
+                initMap();
+            }else{
+                ActivityCompat.requestPermissions(this,
+                        permissions,
+                        LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        }else{
+            ActivityCompat.requestPermissions(this,
+                    permissions,
+                    LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    //
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(TAG, "onRequestPermissionsResult: called.");
+        mLocationPermissionsGranted = false;
+        switch(requestCode){
+            case LOCATION_PERMISSION_REQUEST_CODE:{
+                if(grantResults.length > 0){
+                    for(int i = 0; i < grantResults.length; i++){
+                        if(grantResults[i] != PackageManager.PERMISSION_GRANTED){
+                            mLocationPermissionsGranted = false;
+                            Log.d(TAG, "onRequestPermissionsResult: permission failed");
+                            return;
+                        }
+                    }
+                    Log.d(TAG, "onRequestPermissionsResult: permission granted");
+                    mLocationPermissionsGranted = true;
+                    //initialize our map
+                    initMap();
+                }
+            }
+        }
+    }
+}
